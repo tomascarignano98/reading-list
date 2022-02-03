d9097750382c08b6690679e6b0090d149694e1bb
tomascarignano98 <tomascarignano98@gmail.com>
2022-02-03 20:43:38 -0300

    Deleting data.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,25 @@
+import { doc, deleteDoc } from 'firebase/firestore';
+import { db } from '../firebase/config';
+import trashIcon from '../assets/trashcan.svg';
+
 export default function BookList({ books }) {
   const handleClick = async (id) => {
-    console.log(id);
+    const ref = doc(db, 'books', id);
+    await deleteDoc(ref);
   };
 
   return (
     <div className="book-list">
       <ul>
         {books.map((book) => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>
+          <li key={book.id} style={{ position: 'relative' }}>
             {book.title}
+            <img
+              className="delete-icon"
+              src={trashIcon}
+              alt="delete icon"
+              onClick={() => handleClick(book.id)}
+            />
           </li>
         ))}
       </ul>
