57ab8a4f191a5733f63b04746c9024aff2fc380e
tomascarignano98 <tomascarignano98@gmail.com>
2022-02-03 19:15:52 -0300

    Getting Documents.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
+import { db } from '../firebase/config';
 
 export default function Home() {
-  const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 },
-  ]);
-  console.log(typeof setBooks);
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const ref = collection(db, 'books');
+        const querySnapshot = await getDocs(ref);
+
+        if (querySnapshot.empty) {
+          setBooks([]);
+        } else {
+          const results = [];
+          querySnapshot.forEach((doc) =>
+            results.push({ id: doc.id, ...doc.data() })
+          );
+          setBooks(results);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div>
